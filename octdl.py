95a450848ef354a9015f3b3fad0dc6caa2bf7f88
Sarah Greilsamer <sarahgreilsamer@gmail.com>
2017-08-06 20:16:39 -0700

    Beta 1

--- octdl.py
+++ octdl.py
@@ -17,24 +17,7 @@ from flask import Flask, render_template, send_from_directory
 
 # JUSTIN'S SUBITIZE IMPORTS
 
-from collections import namedtuple
-from datetime import datetime
-from os.path import exists as file_exists, join as join_path
-
-from flask import Flask, render_template, abort, request, send_from_directory, url_for, redirect
-from sqlalchemy.sql.expression import asc, desc
-
-from models import create_session
-from models import Semester
-from models import TimeSlot, Building, Room, Meeting
-from models import Core, Department, Course
-from models import Person
-from models import OfferingMeeting, OfferingCore, OfferingInstructor, Offering
-from models import CourseInfo
-from subitizelib import filter_study_abroad, filter_by_search
-from subitizelib import filter_by_semester, filter_by_department, filter_by_number, filter_by_instructor
-from subitizelib import filter_by_units, filter_by_core, filter_by_meeting, filter_by_openness
-from subitizelib import sort_offerings
+from flask import Flask, render_template, request, send_from_directory
 
 # FOLLOWING CODE TO USE GOOGLE API
 
@@ -78,15 +61,19 @@ def get_options(stories_list):  # use list of lists from get_data
     return stories_dict  # return dictionary of stories
 
 
-#def get_results(options_dict):
-    #
+def get_results(dropdown_options, parameters):
+    results = []
+    for each_dictkey in parameters:
+        if each_dictkey in dropdown_options:
+            results.append(dropdown_options['titles'])
+
 
 
 @app.route('/')
 def view_page():
     stories = get_data(FILE_NAME)  # list of lists
-    dropdown_options = get_options(stories)  # dictionary of stories
-    parameters = request.args.to_dict()  # user's input
+    #dropdown_options = get_options(stories)  # dictionary of stories
+    #parameters = request.args.to_dict()  # user's input
     return render_template('template.html', stories=stories)
 
 
