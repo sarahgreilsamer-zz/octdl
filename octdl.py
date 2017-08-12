7a08236c5aef2dee81fd7155b8ac76ce369af2b8
Sarah Greilsamer <sarahgreilsamer@gmail.com>
2017-08-11 20:19:59 -0700

    Working home page

--- octdl.py
+++ octdl.py
@@ -50,8 +50,16 @@ def get_data(file_name1, file_name2):
     # List of lists
     table1 = sheet1.get_all_values()
     table2 = sheet2.get_all_values()
-    stories = table1[1:]  # Grab all columns
-    stories.append(table2[2:4])  # Only grab the three columns that have relevant information
+    del(table1[0])
+    del(table2[0])
+    for each_story in table2:
+        del(each_story[0])
+        del(each_story[0])
+    stories=[]
+    for each_story in table1:
+        stories.append(each_story)
+    for each_story in table2:
+        stories.append(each_story)
     # Sort by alphabetical order of title
     stories.sort(key=lambda story: story[0])
     return stories  # list of lists
@@ -63,16 +71,23 @@ def get_options(stories_list):  # use list of lists from get_data
     stories_book = []
     stories_origin = []
     stories_link2pdf = []
+    # gathering all possible values for title, book, origin, and link
     for each_story in stories_list:
         stories_title.append(each_story[0])
         stories_book.append(each_story[1])
         stories_origin.append(each_story[2])
         stories_link2pdf.append(each_story[3])
+    # removing duplicates
+    stories_title = list(set(stories_title))
+    stories_book = list(set(stories_book))
+    stories_origin = list(set(stories_origin))
+    stories_link2pdf = list(set(stories_link2pdf))
+    # gather all characteristics into a list of lists
     all_stories.append(stories_title)
     all_stories.append(stories_book)
     all_stories.append(stories_origin)
     all_stories.append(stories_link2pdf)
-    return stories_list  # return list of stories characteristics
+    return all_stories  # return list of stories characteristics
 
 #FIXME
 
@@ -85,18 +100,21 @@ def get_userinput(parameters):  # parameters is the result of args to dict
     return user_inputlist
 
 
-#FIXME
-
-
 def get_results(stories_list, user_inputlist):
     query_result = []
     for each_story in stories_list:  # looking at list of all the stories where each story is a list
         for each_term in (str.split(str.lower(user_inputlist[0]))):  # for each key word entered by the user
             if each_term in str.lower(each_story[0]) or str.lower(each_story[0]) == '':  # if key word in title of story
                 query_result.append(each_story)  # add this story to the results
+    stories_list = query_result
+    query_result = []
+    for each_story in stories_list:  # looking at list of all the stories where each story is a list
         for each_term in (str.split(str.lower(user_inputlist[1]))):
             if each_term in str.lower(each_story[1]) or str.lower(each_story[1]) == '':
                 query_result.append(each_story)
+    stories_list = query_result
+    query_result = []
+    for each_story in stories_list:  # looking at list of all the stories where each story is a list
         for each_term in (str.split(str.lower(user_inputlist[2]))):
             if each_term in str.lower(each_story[2]) or str.lower(each_story[2]) == '':
                 query_result.append(each_story)
@@ -109,10 +127,18 @@ def view_page():
     stories = get_data(FILE_NAME1, FILE_NAME2)  # list of lists [[story 1],[story 2],...]
     stories_list = get_options(stories)  # all options, list of lists [[all titles],[all books],...]
     parameters = request.args.to_dict()  # args to dict, dictionary where keys name of field, and user input are values of keys
-    user_input = get_userinput(parameters) # user's input in a list [title, book, origin]
-    query_results = get_results(stories_list, user_input)  # results of the query
+    if len(parameters) == 0:
+        parameters['title'] = ''
+        parameters['origin'] = ''
+        parameters['book'] = ''
+        query_results = [['N/A', 'N/A', 'N/A', 'N/A']]
+    else:
+        user_input = get_userinput(parameters) # user's input in a list [title, book, origin]
+        query_results = get_results(stories_list, user_input)  # results of the query
     return render_template('template.html', stories=stories, dictionary=str(parameters), query_results=query_results)
 
+
+@app.route('/query-result')
 
 class Story:
     def __init__(self, title, origin, book, link2pdf):
@@ -139,7 +165,3 @@ def view_js(file):
 if __name__ == '__main__':
     chdir(dirname(realpath(expanduser(__file__))))
     app.run(debug=True)
-    stories = get_data(FILE_NAME1, FILE_NAME2)  # list of lists [[story 1],[story 2],...]
-    stories_list = get_options(stories)  # all options, list of lists [[all titles],[all books],...]
-    user_input = get_userinput()  # user's input in a list [title, book, origin]
-    results = get_results(stories_list, user_input)  # results of the query
